5d41a47f09105d13f3aaeabbe03ad33e40eca043
Alex Chukrii <chukrii.alex@gmail.com>
2024-01-26 22:43:16 +0100

    Added config service, Added as provider AUTH_SERVICE in api, connected to RabbitMQ in Auth main.ts

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('AUTH_SERVICE') private AuthService: ClientProxy,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getUser() {
+    return this.AuthService.send(
+      {
+        cmd: 'get-user',
+      },
+      {},
+    );
   }
 }
