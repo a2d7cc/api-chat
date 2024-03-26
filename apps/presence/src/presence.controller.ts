ef2f970e31e6053d42a02d2f9435278598e8222a
Alex Chukrii <chukrii.alex@gmail.com>
2024-03-26 10:39:03 +0100

    Using AuthGuard

--- apps/presence/src/presence.controller.ts
+++ apps/presence/src/presence.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get } from '@nestjs/common';
 import { PresenceService } from './presence.service';
 import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
 import { SharedService } from '@app/shared';
+import { AuthGuard } from '@app/shared/auth.guard';
 
 @Controller()
 export class PresenceController {
   constructor(
     private readonly presenceService: PresenceService,
     private readonly sharedService: SharedService,
+    // TEMP
+    private readonly authGuard: AuthGuard,
   ) {}
 
   @Get()
@@ -18,6 +21,7 @@ export class PresenceController {
   @MessagePattern({ cmd: 'get-presence' })
   async getUser(@Ctx() context: RmqContext) {
     this.sharedService.acknowledgeMessage(context);
+    console.log('jwt', this.authGuard.hasJWT());
 
     return this.presenceService.getHello();
   }
