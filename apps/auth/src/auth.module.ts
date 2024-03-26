57d82861239f6284e4e28988f353074a93b901ac
Alex Chukrii <chukrii.alex@gmail.com>
2024-03-26 10:27:45 +0100

    Fixed export from sharedModule

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
 import { SharedModule } from '@app/shared';
