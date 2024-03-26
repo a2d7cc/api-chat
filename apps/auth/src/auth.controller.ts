77a1a49aa7a1ccdd863131572edbe4fef530de58
Alex Chukrii <chukrii.alex@gmail.com>
2024-03-26 09:58:27 +0100

    Created acknowledge Method in sharedService and the used it in auth, presence controller

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,31 +1,37 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
+import { SharedService } from '@app/shared';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
-  }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly sharedService: SharedService,
+  ) {}
 
   @MessagePattern({ cmd: 'get-users' })
   async getUser(@Ctx() context: RmqContext) {
-    const channel = context.getChannelRef();
+    this.sharedService.acknowledgeMessage(context);
+    /*     const channel = context.getChannelRef();
     const message = context.getMessage();
-    channel.ack(message);
+    channel.ack(message); */
 
     return this.authService.getUsers();
   }
 
   @MessagePattern({ cmd: 'post-user' })
   async postUser(@Ctx() context: RmqContext) {
-    const channel = context.getChannelRef();
+    this.sharedService.acknowledgeMessage(context);
+    /*     const channel = context.getChannelRef();
     const message = context.getMessage();
-    channel.ack(message);
+    channel.ack(message); */
 
     return this.authService.postUser();
+  }
+
+  @Get()
+  getHello(): string {
+    return this.authService.getHello();
   }
 }
