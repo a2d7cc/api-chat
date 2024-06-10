49a240acf37e12df2e3ae4a16646a20086d59372
Alex Chukrii <chukrii.alex@gmail.com>
2024-06-10 15:08:00 +0200

    Created base.abstract.repository.ts

--- libs/shared/src/repositories/base/base.interface.repository.ts
+++ libs/shared/src/repositories/base/base.interface.repository.ts
@@ -2,7 +2,7 @@ import { DeepPartial, FindManyOptions, FindOneOptions } from 'typeorm';
 
 export interface BaseInterfaceRepository<T> {
   create(data: DeepPartial<T>): T;
-  createMany(data: DeepPartial<T>[]): T;
+  createMany(data: DeepPartial<T[]>): T;
   save(data: DeepPartial<T>): Promise<T>;
   saveMany(data: DeepPartial<T>[]): Promise<T[]>;
   findById(id: number): Promise<T>;
