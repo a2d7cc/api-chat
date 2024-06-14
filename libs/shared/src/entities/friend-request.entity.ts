dfc06271ce696ae03c2a3752dbde9e3983a5926e
Alex <a2d7cc@gmail.com>
2024-06-14 12:28:07 +0200

    Added friend-request entity and relation to this entity from user

---
+++ libs/shared/src/entities/friend-request.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+import { UserEntity } from './user.entity';
+
+@Entity('friend-request')
+export class FriendRequestEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => UserEntity, (user) => user.friendRequestCreator)
+  creator: UserEntity;
+
+  @ManyToOne(() => UserEntity, (user) => user.friendRequestReceiver)
+  receiver: UserEntity;
+}
