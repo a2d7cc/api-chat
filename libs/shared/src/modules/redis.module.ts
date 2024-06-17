c988c87027797a200948306604df8f75fe65da85
Alex Chukrii <chukrii.alex@gmail.com>
2024-06-17 15:06:46 +0200

    Registred CacheModule

--- libs/shared/src/modules/redis.module.ts
+++ libs/shared/src/modules/redis.module.ts
@@ -1,6 +1,13 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-    
+  imports: [
+    CacheModule.register({
+      max: 100,
+      ttl: 0,
+    }),
+  ],
 })
 export class RedisModule {}
