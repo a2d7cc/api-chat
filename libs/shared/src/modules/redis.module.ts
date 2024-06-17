01a44aa231e7116d58afd020c5981de0d13e461d
Alex Chukrii <chukrii.alex@gmail.com>
2024-06-17 16:08:51 +0200

    COnfigured redis for CacheModule

--- libs/shared/src/modules/redis.module.ts
+++ libs/shared/src/modules/redis.module.ts
@@ -2,13 +2,16 @@ import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { RedisService } from '../services/redis.service';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
     CacheModule.registerAsync({
       useFactory: async (configService: ConfigService) => ({
-        ttl: 0,
-        max: 100,
+        store: await redisStore({
+          url: configService.get('REDIS_URI'),
+          ttl: 0,
+        }),
       }),
       isGlobal: true,
       inject: [ConfigService],
